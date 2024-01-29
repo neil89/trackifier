fec42528efded3fc2efe8bcad30de290a68929c4
neil89 <daniel.rodriguez.molina@gmail.com>
2024-01-29 23:11:49 +0100

    🔨 Adding primeng, ngx-translate and angular/fire

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Trackifier
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.3.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    provideFirebaseApp(() => initializeApp({"projectId":"trackifier","appId":"1:231128310965:web:e65a10ed747030bd4b4afc","storageBucket":"trackifier.appspot.com","apiKey":"AIzaSyBW4-TBUDUqreS52SmVh_h9y7xVf_LRUHk","authDomain":"trackifier.firebaseapp.com","messagingSenderId":"231128310965"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions())
   ],
   providers: [],
   bootstrap: [AppComponent]
